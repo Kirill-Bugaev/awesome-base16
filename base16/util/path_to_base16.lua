415afbddc7d4091aa9893560dfdf1317b9941cbb
Kirill Bugaev <kirill.bugaev87@gmail.com>
2019-08-31 22:14:36 +0800

    lainmod update

--- base16/util/path_to_base16.lua
+++ base16/util/path_to_base16.lua
@@ -3,4 +3,3 @@ local base16_location = script_location:sub(1, script_location:find("/[^/]+/$"))
 -- if stazza above doesn't work properly just hardcode path
 --local base16_location = "/home/user1/.config/awesome/base16/"
 return base16_location
-
